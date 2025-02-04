d91c68fc5ae7f2f71ac2d17c34e46eb195b28333
Top <79712222+Ricesucooker@users.noreply.github.com>
2025-02-04 09:04:15 +0000

    updated shell script

--- getlog.sh
+++ getlog.sh
@@ -4,10 +4,19 @@
 var_Hostname=$(hostname)
 echo $var_Hostname
 
+
+if [ -d ./$hostname ]; then
+
+rm -rf "./$var_Hostname"
+else
+mkdir -p "./$var_Hostname"
+fi 
+
+
 echo "Getting log for $var_Hostname"
 
 sleep 3
-mkdir -p "./$var_Hostname"
+
 uname -a > "./$var_Hostname/$var_Hostname.txt"
 ldd --version >> "./$var_Hostname/$var_Hostname.txt"
 mkdir -p "./$var_Hostname/Agent"
@@ -20,10 +29,18 @@ sleep 1
 
 journalctl > "./$var_Hostname/Journalctl.txt"
 
+dmesg | grep ltechagent > "./$var_Hostname/LTdmesg.txt"
+dmesg > "./$var_Hostname/dmesg.txt"
+
 echo "completed copying system logs.."
 
 
+if [ -d ./$hostname.tar.gz ]; then
+
+rm -rf "./$var_Hostname.tar.gz"
+else
 tar -czvf ./$var_Hostname.tar.gz ./$var_Hostname
+fi 
 
 echo "compress completed"
 
