6583371cc9ff249cbe15e8b855d0a15ddc4927a2
Top <79712222+Ricesucooker@users.noreply.github.com>
2025-02-07 13:12:21 +0000

    very easy tool to use with screen connect

---
+++ Janusfinder.ps1
@@ -0,0 +1,19 @@
+﻿New-Item -ItemType "directory" -Path "C:\AutoJanuslog" -ErrorAction SilentlyContinue
+
+[String]$searchStr = "Janus.Cryptography.RemoteAgentSettingsCrypto"
+ 
+
+$LTlogFile = "C:\Windows\LTSvc\LTErrors.txt"
+
+
+
+if (Get-Content -Path $LTlogFile -tail 50 | ForEach-Object{ $_.Trim() } | Where-Object {$_.Contains($searchStr.Trim())}){
+
+                Write-Host "Janus Error Found! Please reinstall LTAgent and collect logs!" | Write-Output
+                $downloadURL = "https://utilities.itsupport247.net/pstautomation/CWAutoLogCollector.exe"
+                $downloadPath = "C:\AutoJanuslog\CWAutoLogCollector.exe"
+                $unblockURL = $downloadPath
+
+                Invoke-WebRequest -Uri $downloadURL -OutFile $downloadPath
+                Unblock-File -Path $downloadPath
+                Start-Process -FilePath $downloadPath -Verb RunAs  } else { Write-Host "Agent Online" | Write-Output }
