230bcf71e4e90b470f2c7dac5a3280c04b22df3d
Top <79712222+Ricesucooker@users.noreply.github.com>
2025-02-07 11:25:00 +0000

    upload sh

---
+++ Linux agent/linux.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+varLTfolder="/usr/local/ltechagent"
+
+rm -rf "$/varLTfolder/ltechagent"
+rm -rf "$/varLTfolder/libltech.so"
+
+
+cp ./libltech.so $varLTfolder
+cp ./ltechagent $varLTfolder
